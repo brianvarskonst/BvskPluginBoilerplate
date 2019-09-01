dd678ebcf10dc3bece46d97ed330b4e53ca96a40
brianvarskonst <brianvar.skonst@gmail.com>
2019-09-01 13:12:40 +0200

    Debug Tests - TestBootstrap & UsedClassesAvailableTest

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -7,9 +7,11 @@ $projectDir = static function (): string {
     if (isset($_SERVER['PROJECT_ROOT']) && file_exists($_SERVER['PROJECT_ROOT'])) {
         return $_SERVER['PROJECT_ROOT'];
     }
+
     if (isset($_ENV['PROJECT_ROOT']) && file_exists($_ENV['PROJECT_ROOT'])) {
         return $_ENV['PROJECT_ROOT'];
     }
+
     $dir = $rootDir = __DIR__;
 
     while (!file_exists($dir . '/.env')) {
@@ -26,9 +28,8 @@ $projectDir = static function (): string {
 define('TEST_PROJECT_DIR', $projectDir());
 
 $loader = require TEST_PROJECT_DIR . '/vendor/autoload.php';
-require_once __DIR__ . '/../vendor/autoload.php';
-
 KernelLifecycleManager::prepare($loader);
+require_once __DIR__ . '/../vendor/autoload.php';
 
 if (!class_exists(Dotenv::class)) {
     throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
--- tests/UsedClassesAvailableTest.php
+++ tests/UsedClassesAvailableTest.php
@@ -29,7 +29,7 @@ class UsedClassesAvailableTest extends TestCase
     private function getPluginClasses(): Finder
     {
         $finder = new Finder();
-        $finder->in(realpath(__DIR__ . '/tests/'));
+        $finder->in(realpath(__DIR__ . '/Tests/'));
         $finder->exclude('Test');
 
         return $finder->files()->name('*.php');
